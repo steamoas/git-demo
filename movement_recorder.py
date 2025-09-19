941df8fcd14d7be7f9c4f98977ab39d7b36df5db
shaggy <gabriel.couchenour@gmail.com>
2025-09-18 21:38:41 -0500

    Create movement_recorder.py

---
+++ movement_recorder.py
@@ -0,0 +1,60 @@
+from pybricks.hubs import PrimeHub
+from robot import drive_base
+from robot import hub
+from pybricks.robotics import DriveBase
+from pybricks.parameters import Stop, Color, Button
+from pybricks.tools import wait, run_task
+
+#configure args at the bottom of the file
+
+def track_movement(drive_base: DriveBase, hub: PrimeHub, drive_base_name: str = "drive_base", rounding_digits: int = 1, stop_type: Stop = Stop.HOLD):
+    hub.light.on(Color.GREEN)
+    hub.system.set_stop_button((Button.LEFT, Button.RIGHT))
+    drive_base.use_gyro(True)
+    print("Welcome to the movement tracking program!")
+    print("To begin tracking, press your hub's center button.")
+    print("When you want to finish tracking, press the button again. Your movements will be recorded to the console.")
+    print("To generate python instructions for your tracked movements, press the bluetooth button.")
+    print("To exit, press the left and right arrow buttons simultaneously")
+
+    actions = []
+    actions_count = 0
+
+    while True:
+        buttons = hub.buttons.pressed()
+        if Button.CENTER in buttons:
+
+            hub.light.on(Color.RED)
+            drive_base.reset()
+            drive_base.stop()
+
+            wait(250)
+
+            while not Button.CENTER in hub.buttons.pressed():
+                wait(100)
+
+            end_angle = drive_base.angle()
+            end_distance = drive_base.distance()
+
+            if end_angle * 10 > end_distance:
+                actions.append((0, end_angle))
+                print(f"{actions_count}: turn {end_angle} degrees")
+            else:
+                actions.append((1, end_distance))
+                print(f"{actions_count}: drive {end_distance} mm")
+
+            actions_count += 1
+            hub.light.on(Color.GREEN)
+            wait(250)
+
+
+        elif Button.BLUETOOTH in buttons:
+            print("Generating python code:\n")
+            suffix = f", then = {stop_type}" if stop_type != Stop.HOLD else ""
+            for i in range(actions_count):
+                if actions[i][0] == 0:
+                    print(f"{drive_base_name}.turn({round(actions[i][1], rounding_digits)}{suffix})")
+                else:
+                    print(f"{drive_base_name}.straight({actions[i][1]}{suffix})")
+            break
+run_task(track_movement(drive_base, hub))
