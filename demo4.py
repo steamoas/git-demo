9a3c1ec677d7fe7a1c6299539cda6990e00b91a1
steam <steam@ozarkschool.org>
2025-09-23 17:12:13 -0500

    9/23

---
+++ demo4.py
@@ -0,0 +1,4 @@
+from robot2 import drive, left_drive, right_drive
+
+drive.straight(280)
+drive.turn(95)
---
+++ robot2.py
@@ -0,0 +1,13 @@
+from pybricks.hubs import PrimeHub
+from pybricks.robotics import DriveBase
+from pybricks.pupdevices import Motor, ColorSensor
+from pybricks.parameters import Button, Port, Direction
+
+hub = PrimeHub()
+#Adjust these parameters to fit your robot. Also ensure that you add any sensors here.
+
+left_drive = Motor(Port.D, Direction.COUNTERCLOCKWISE)
+right_drive = Motor(Port.F, Direction.CLOCKWISE)
+
+drive = DriveBase(left_drive, right_drive, 60, 104)
+drive.use_gyro(True)
